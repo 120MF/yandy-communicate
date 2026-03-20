8cb6714e0595e5b125e6db9b1ad8376cf70b5a91
WindWeaver <moonbite233@gmail.com>
2026-03-20 16:19:24 +0800

    init commit

---
+++ include/YandyCommunicateData.hpp
@@ -0,0 +1,13 @@
+#ifndef YANDYCOMMUNICATEDATA_HPP_
+#define YANDYCOMMUNICATEDATA_HPP_
+
+struct YandyCommunicateData {
+    int value;
+
+    // Optional: implement format() for debugging
+    // std::string format() const {
+    //     return std::format("value: {}", value);
+    // }
+};
+
+#endif // YANDYCOMMUNICATEDATA_HPP_
